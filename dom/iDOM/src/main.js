e1c445961fa9a7d155fb125e1181b6b24dec5f88
JackSun <1716118719@qq.com>
2021-05-30 17:23:49 +0800

    Auto Update

--- dom/iDOM/src/main.js
+++ dom/iDOM/src/main.js
@@ -1,4 +1,9 @@
 console.log("开始执行main");
-console.log(dom.parent(s2));
-console.log(dom.previous(s2, true))
+// console.log(dom.parent(s2));
+// console.log(dom.previous(s2, true))
 console.log("现在可以在控制台执行新的操作");
+// const div = dom.find('#test>.red')[0] // 获取对应的元素
+// dom.style(div, 'color', 'red') // 设置 div.style.color
+
+// const divList = dom.find('.red') // 获取多个 div.red 元素
+// dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
